5ca5342f38bfcbe22713b33a0405ca1107f8aada
tooxoot <sv30192+git@gmail.com>
2017-10-08 23:14:56 +0200

    {#7} Reworket model's structure

--- src/app/shared/data-model/svg.model.ts
+++ src/app/shared/data-model/svg.model.ts
@@ -1,14 +1,31 @@
-export class ModelElement {
-    constructor(
-        public points: [number, number][],
-        public id: string,
-        public filled: boolean,
-        public outlined: boolean,
-        public closed: boolean
-    ) {};
-
-    public getElements(): HTMLElement[] {
-         return [document.getElementById(this.id + '_FILLING'), document.getElementById(this.id + '_OUTLINE')];
-    }
-
+export interface XY {
+    X: number;
+    Y: number;
 }
+
+export interface GraphicValues {
+    fill: string;
+    stroke: string;
+    strokeWidth: string;
+}
+
+export interface ModelElement {
+    points: XY[];
+    id: number;
+    filled: boolean;
+    outlined: boolean;
+    closed: boolean;
+    graphicValues?: GraphicValues;
+}
+
+class Model {
+    readonly elements: ModelElement[] = [];
+
+    public getElement(id: number): ModelElement {
+        return this.elements.find( (e) => {
+            return e.id === id
+        } );
+    }
+}
+
+export const MODEL = new Model();
