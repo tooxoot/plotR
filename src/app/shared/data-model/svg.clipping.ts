af40e2d54d13d7fad0dd8e9e46ec0fb94883d6de
tooxoot <sv30192+git@gmail.com>
2017-11-18 15:02:41 +0100

    {#13,#8} Added module and exports! - The subjects will not be altered - Documentation added

--- src/app/shared/data-model/svg.clipping.ts
+++ src/app/shared/data-model/svg.clipping.ts
@@ -2,84 +2,91 @@ import {ModelElement} from './svg.model'
 import ClipperLib = require('clipper-lib');
 import {XY, ClipStruct} from '../types'
 
+/**
+ * This module contains functions which take care of clipping ModelElements.
+ */
+export module ClipUtils {
 
-class ClipResult {
-    constructor(
-        public points: [number, number][],
-        public closed: boolean
-    ) {};
-}
+    /**
+     * Clips each of the subjects' ModelElements with the upper elements.
+     * @return An array consisting of the clipping results.
+     */
+    export function clip(subjects: ModelElement[]): ModelElement[] {
+        const solution: ModelElement[] = [];
 
-
-export function clip(model: ModelElement[]): ModelElement[] {
-    const solution: ModelElement[] = [];
-    const subjects: ModelElement[] = model;
-
-    for (let i = 0; i < subjects.length - 1; i++) {
-        if (!subjects[i].outlined) { continue; }
-        console.log('>>>next_subject')
-        solution.push(...clipWithUpper(subjects[i], subjects.slice(i + 1)));
-    }
-
-    solution.push(subjects.pop())
-
-    return solution;
-}
-
-function clipWithUpper(subject: ModelElement, clips: ModelElement[]): ModelElement[] {
-    if (subject.closed) {
-        subject.points.push(subject.points[0]);
-    }
-
-    let solution: ModelElement[] = [subject];
-
-    for (let i = 0; i < clips.length; i++) {
-        if (solution === []) { break; }
-        const currentClip = clips[i];
-        if (currentClip.filled) { solution = clipSubjects(solution, currentClip); }
-    }
-
-    return solution;
-}
-
-function clipSubjects(subjects: ModelElement[], clip: ModelElement): ModelElement[] {
-    let solution: ModelElement[] = [];
-
-    subjects.forEach(subject => {
-        const temp = clipTwo(subject, clip)
-        console.log(temp)
-        solution = solution.concat(temp)
-    });
-
-    return solution;
-}
-
-function clipTwo(subject: ModelElement, clip: ModelElement, regardClosing: boolean = false): ModelElement[] {
-    const clipper = new ClipperLib.Clipper();
-    const solutionTree = new ClipperLib.PolyTree();
-
-    // console.lo
-
-    clipper.AddPath(subject.points, ClipperLib.PolyType.ptSubject, regardClosing && subject.closed);
-    clipper.AddPath(clip.points, ClipperLib.PolyType.ptClip, true);
-
-    clipper.Execute(  ClipperLib.ClipType.ctDifference,
-                      solutionTree,
-                      ClipperLib.PolyFillType.pftNonZero,
-                      ClipperLib.PolyFillType.pftNonZero
-                    );
-
-    const result = regardClosing && subject.closed ?
-            ClipperLib.Clipper.ClosedPathsFromPolyTree(solutionTree) :
-            ClipperLib.Clipper.OpenPathsFromPolyTree(solutionTree);
-
-    return result.map(element => {
-        return {
-            points: element,
-            closed: regardClosing && subject.closed,
-            filled: regardClosing && subject.filled,
-            outlined: subject.outlined,
-            id: subject.id,
+        for (let i = 0; i < subjects.length - 1; i++) {
+            if (!subjects[i].outlined) { continue; }
+            solution.push(...clipMultipleClips(subjects[i], subjects.slice(i + 1)));
         }
-    });
+
+        solution.push(subjects[subjects.length - 1])
+
+        return solution;
+    }
+
+    /**
+     * Clips the subject with each of the ModelElements in upperElements.
+     * @return An array consisting of the clipping results.
+     */
+    export function clipMultipleClips(subject: ModelElement, clippingElements: ModelElement[]): ModelElement[] {
+        let solution: ModelElement[] = [subject];
+
+        for (let i = 0; i < clippingElements.length; i++) {
+            if (solution === []) { break; }
+            const currentClip = clippingElements[i];
+            if (currentClip.filled) { solution = clipMultipleSubjects(solution, currentClip); }
+        }
+
+        return solution;
+    }
+
+    /**
+     * Clips each of the subjects' ModelElements with the clippingElements.
+     * @return An array consisting of the clipping results.
+     */
+    export function clipMultipleSubjects(subjects: ModelElement[], clippingElement: ModelElement): ModelElement[] {
+        let solution: ModelElement[] = [];
+
+        subjects.forEach(subject => {
+            const temp = clipTwo(subject, clippingElement)
+            solution = solution.concat(temp)
+        });
+
+        return solution;
+    }
+
+    /**
+     * Clips the subject with the clippingElement.
+     * @param regardClosing If true and the subject is closed all clipping results will be closed!
+     *                      With this option enabled holes are possible!
+     * @return An array consisting of the clipping results.
+     */
+    export function clipTwo(subject: ModelElement, clippingElement: ModelElement, regardClosing: boolean = false): ModelElement[] {
+        const clipper = new ClipperLib.Clipper();
+        const solutionTree = new ClipperLib.PolyTree();
+        const clippingPoints = subject.closed ? subject.points.concat(subject.points[0]) : subject.points;
+
+        clipper.AddPath(clippingPoints, ClipperLib.PolyType.ptSubject, regardClosing && subject.closed);
+        clipper.AddPath(clippingElement.points, ClipperLib.PolyType.ptClip, true);
+
+        clipper.Execute(  ClipperLib.ClipType.ctDifference,
+                        solutionTree,
+                        ClipperLib.PolyFillType.pftNonZero,
+                        ClipperLib.PolyFillType.pftNonZero
+                        );
+
+        const result = regardClosing && subject.closed ?
+                ClipperLib.Clipper.ClosedPathsFromPolyTree(solutionTree) :
+                ClipperLib.Clipper.OpenPathsFromPolyTree(solutionTree);
+
+        return result.map(element => {
+            return {
+                points: element,
+                closed: regardClosing && subject.closed,
+                filled: regardClosing && subject.filled,
+                outlined: subject.outlined,
+                id: subject.id,
+            }
+        });
+    }
 }
