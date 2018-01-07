67c304208c9bfeb48fedc40978c045c7749693c5
tooxoot <sv30192+git@gmail.com>
2018-01-07 21:04:55 +0100

    {#16} Added ellipse processor

---
+++ src/app/shared/svg-processor/svg.ellipse.processor.ts
@@ -0,0 +1,22 @@
+import {XY} from '../data-model/svg.model'
+import {ChildResult} from 'app/shared/svg-processor/svg.input.processor';
+
+export function processEllipseElement(cx: number, cy: number, rx: number, ry: number): ChildResult {
+    const polyPoints: XY[] = [];
+
+    for (let angle = 0; angle < 360; angle += 5) {
+        const radians = (Math.PI / 180) * angle
+        const cos = Math.cos(radians)
+        const sin = Math.sin(radians)
+
+        polyPoints.push({
+            X: rx * cos + cx,
+            Y: ry * sin + cy
+        });
+    }
+
+    return {
+        points: polyPoints,
+        closed: true
+    }
+}
