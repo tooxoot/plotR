7dedda9a6e40d2b0a2b73e7c04b4c8d0fae2d60e
tooxoot <sv30192+git@gmail.com>
2018-03-22 20:37:00 +0100

    {#18} Added input fields and state to line filling component

--- src/react-components/svg.line.filling.input.tsx
+++ src/react-components/svg.line.filling.input.tsx
@@ -5,20 +5,66 @@ import { LineFilling } from '../redux-model/redux.line.filling';
 
 interface Props {
     selectedIds: number[];
-    // tslint:disable: no-any
+    // tslint:disable:next-line no-any
     fill: (args: LineFilling.Arguments) => any;
-    // tslint:enable: no-any
 }
 
-const FILL_INPUT_COMPONENT: React.SFC<Props> = ({selectedIds, fill}) => {
-    const fillAll = () => selectedIds.forEach(id => fill({id, angle: 45, offset: 0, spacing: 5000}));
+interface State {
+    angle: number;
+    offset: number;
+    spacing: number;
+}
 
-    return (
-        <div>
-            <button onClick={() => fillAll()}> FILL </button>
-        </div>
-    );
-};
+class FillComponent extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            angle: 45,
+            offset: 0,
+            spacing: 5000,
+        };
+      }
+
+    render() {
+        const fillAll = () => this.props.selectedIds.forEach(
+            id => this.props.fill({
+                id,
+                ...this.state
+            })
+        );
+
+        return (
+            <div>
+                Angle:
+                <input
+                    className="angle-input"
+                    type="number"
+                    value={this.state.angle}
+                    onChange={(e) => {
+                        this.setState({angle: +(e.currentTarget.value) % 180});
+                        console.log(this.state);
+                    }
+                    }
+                />
+                Offset:
+                <input
+                    className="offset-input"
+                    type="number"
+                    value={(this.state.offset)}
+                    onChange={(e) => this.setState({offset: Math.max(+(e.currentTarget.value), 0)})}
+                />
+                Spacing:
+                <input
+                    className="spacing-input"
+                    type="number"
+                    value={this.state.spacing}
+                    onChange={(e) => this.setState({spacing: Math.max(+(e.currentTarget.value), 1)})}
+                />
+                <button onClick={fillAll}> FILL </button>
+            </div>
+        );
+    }
+}
 
 const stateToProps = (state: ReduxState) => {
     return {
@@ -35,4 +81,4 @@ const  dispatchToProps = (dispatch: ReactRedux.Dispatch<ReduxState>) => (
 export const FILL_INPUT = ReactRedux.connect(
     stateToProps,
     dispatchToProps
-)(FILL_INPUT_COMPONENT);
+)(FillComponent);
