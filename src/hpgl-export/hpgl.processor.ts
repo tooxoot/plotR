7ebae713028b49f37bc321bef4046156e2bd8d4d
tooxoot <sv30192+git@gmail.com>
2018-10-08 18:49:09 +0200

    {#12, #8} Refactored and improved the hpgl processor

--- src/hpgl-export/hpgl.processor.ts
+++ src/hpgl-export/hpgl.processor.ts
@@ -1,6 +1,58 @@
 import { XY } from '../data-model/svg.model';
 import { TreeTypes as TT } from '../data-model/model.tree.types';
+import { MathUtils as MU } from '../utils/math.utils';
+
 export module HPGLUtils {
+
+    const apply = (scaling: Scaling) => {
+        const xFactor = scaling.outputDimensions.X / scaling.inputDimensions.X;
+        const yFactor = scaling.outputDimensions.Y / scaling.inputDimensions.Y;
+
+        const scalingFunction = MU.withXYArgs(
+            MU.concat(
+                MU.scale([xFactor, yFactor]),
+                MU.translate([scaling.offset.X, scaling.offset.Y])
+            )
+        );
+
+        return (points: XY[]) => points.map(scalingFunction);
+    };
+
+    const concatFirstPointIf = (closed: boolean[]) =>
+        (path: XY[], idx: number) => closed[idx] ? path.concat(path[0]) : path;
+
+    const toStrings = (p: XY) => `${p.X},${p.Y}`;
+
+    const assignCommands = (pString: string, idx: number) => (
+        idx === 0 ? 'PU' :
+        idx === 1 ? 'PD' :
+        '' ) + pString;
+
+    const addJoints = (pString: string, idx: number, arr: string[]) => pString  + (idx === arr.length - 1 ? ';' : ',');
+
+    const join20 = (lines: string[], pString: string, idx: number) => {
+        if (idx % 20 === 0) {
+            return [...lines, pString];
+        }
+
+        return [...lines.slice(0, -1), lines.slice(-1) + pString];
+    };
+
+    const toHpglLines = (hpglStrings: string[], path: XY[]) => [
+        ...hpglStrings,
+        ...path
+            .map(toStrings)
+            .map(assignCommands)
+            .map(addJoints)
+            .reduce(join20, [])
+    ];
+
+    const convertNodes = (scaling: Scaling) => ({paths, closed}: TT.DrawableNode) =>
+        paths
+            .map(apply(scaling))
+            .map(concatFirstPointIf(closed))
+            .reduce(toHpglLines, [])
+            .map(line => line + '\n');
 
     /**
      * This interface defines the scaling between the model and the plotter's coordinate System
@@ -16,78 +68,12 @@ export module HPGLUtils {
      * applies the provided scaling and creates initial overhead.
      */
     export function convertToHPGL(drawables: TT.DrawableNode[], scaling: Scaling): string[] {
-        const result: string[] = [];
+        const initialization = ['IN;', 'VS1;', 'SP1;'];
+        const convert = convertNodes(scaling);
 
-        result.push('IN;');
-        result.push('VS1;');
-        result.push('SP1;');
-
-        drawables.reduce(
-            (hpglStrings, drawable) => {
-                return hpglStrings.concat(
-                    ...drawable.paths.map((points, idx) => convertNode(points, drawable.closed[idx], scaling))
-                );
-            },
-            result
+        return drawables.reduce(
+            (hpglLines, drawable) => hpglLines.concat(convert(drawable)),
+            initialization
         );
-
-        return result;
-    }
-
-    /**
-     * This function converts the provided ModelElement)s to HPGL output,
-     *  and applies the provided scaling.
-     */
-    function convertNode(points: XY[], closed: boolean, scaling: Scaling): string[] {
-        const result: string[] = [];
-        const scaledPoints = points.map(point => scale(point, scaling));
-        const firstPoint = scaledPoints[0];
-        let pathString = 'PD';
-
-        result.push(`PU${firstPoint.X},${firstPoint.Y};`);
-        result.push('PD;');
-
-        scaledPoints.forEach((point, idx) => {
-            if (idx === 0) { return; }
-
-            pathString += `${point.X},${point.Y}`;
-
-            if (idx % 20 === 0) {
-                pathString += ';';
-                result.push(pathString);
-                pathString = 'PD';
-                return;
-            }
-
-            if (idx === scaledPoints.length - 1) {
-                pathString += ';';
-            } else {
-                pathString += ',';
-            }
-        });
-
-        if (closed) {
-            pathString += `PD${scaledPoints[0].X},${scaledPoints[0].Y};`;
-        }
-
-        result.push(pathString);
-        result.push('PU;');
-
-        return result;
-    }
-
-    /**
-     * Applies scaling to given point.
-     */
-    function scale(point: XY, scaling: Scaling): XY {
-        const xFactor = scaling.outputDimensions.X / scaling.inputDimensions.X;
-        const yFactor = scaling.outputDimensions.Y / scaling.inputDimensions.Y;
-        const xOut = xFactor * point.X + scaling.offset.X;
-        const yOut = yFactor * point.Y + scaling.offset.Y;
-
-        return {
-            X: Math.round(xOut),
-            Y: Math.round(yOut)
-        };
     }
 }
